a7bbe1acee3de0fc0ff28f0b58e386d1c99d70e7
Bikranta Bista <bik@Bikrantas-MacBook-Air.local>
2021-03-14 00:26:52 -0500

    initial edits w/ bubblesort

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [data, setData] = useState([]);
+  const [arrayField, setArrayField] = useState('');
+  const [indices, setIndices] = useState({
+    i: 0,
+    j: 0,
+  });
+
+  const handleChange = (event) => {
+    //controls input field
+    let field = event.target.value
+    setArrayField(field);
+
+    //converts input field to array
+    let array = field.replace(/\s/g, '').split(',');
+    array = array.map(num => parseInt(num));
+    setData(array);
+    setIndices({i:0, j:0});
+  }
+
+
+  const nextItr = () => {
+
+    let arr = [...data];
+
+    let i = indices.i;
+    let j = indices.j;
+
+    //check i
+    if (i < arr.length){
+      
+      //check j
+      if (j < arr.length - i - 1){
+        //if greater, swap
+        if (arr[j] > arr[j+1]) {
+          let temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          setData(arr);
+        }
+        setIndices({i, j:j+1})
+      }
+      //else reset j and increment i
+      else{
+        setIndices({i:i+1, j: 0})
+      }
+    }
+    else {
+      console.log('Done')
+    }
+
+  }
+
+  const prevItr = () => {
+
+    let arr = [...data];
+
+    let i = indices.i;
+    let j = indices.j;
+
+    //check i
+    if (i >= 0){
+      
+      //check j
+      if (j > arr.length - i - 1){
+        //if greater, swap
+        if (arr[j] > arr[j-1]) {
+          let temp = arr[j];
+          arr[j] = arr[j-1];
+          arr[j-1] = temp;
+          setData(arr);
+        }
+        setIndices({i, j:j-1})
+      }
+      //else reset j and increment i
+      else{
+        setIndices({i:i-1, j: 0})
+      }
+    }
+    else {
+      console.log('Done')
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{textAlign: 'center', width: 'auto'}}>
+      <h1>Bubble Sort</h1>
+      <div>
+        <form>
+          <label>
+            Enter values to sort (separate with commas):
+            <input type="text" value={arrayField} 
+            onChange={
+              (e) => {
+                handleChange(e);
+              }} />
+          </label>
+          <button onClick={(e)=>{
+            e.preventDefault();
+            nextItr();
+          }}>Next</button>
+          <button onClick={(e)=>{
+            e.preventDefault();
+            prevItr();
+          }}>Prev</button>
+        </form>
+      </div>
+      <div>
+      {
+        data.map((item, idx) => {
+          if (item) {
+            let color = (idx === indices.j || idx === indices.j+1) ? 'green' : 'grey';
+            if (idx > data.length - indices.i - 1) color = 'red';
+
+            return <div style={{backgroundColor: color, width: '30px', marginRight: '10px', height: `${item*10}px`, float: 'left'}}>{item}</div>
+          }
+        })
+      }
+      </div>
+      
     </div>
   );
 }
